4bdd57fc3480ad31936e529bc5eafe298e28cd01
Kishan2029 <63163246+Kishan2029@users.noreply.github.com>
2021-04-16 16:14:20 +0530

    Header Component is added

--- src/App.js
+++ src/App.js
@@ -2,23 +2,20 @@ import React from 'react'
 import {Switch,Route} from 'react-router-dom'
 import Homepage from './Pages/Homepage/Homepage'
 import ShopPage from './Pages/ShopPage/ShopPage'
+import Header from './Components/Header/Header'
 import './App.css'
 
 
 
-const HatsPage= () =>(
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
           <Route exact path='/' component={Homepage}/>
           <Route path='/shop' component={ShopPage}/>
-          {/* <Homepage/> */}
+
       </Switch>
         
     </div>
--- src/Pages/ShopPage/ShopPage.js
+++ src/Pages/ShopPage/ShopPage.js
@@ -1,27 +1,41 @@
-import React from 'react'
+import React,{useState} from 'react'
 import SHOP_DATA from './ShopData'
 import CollectionPreview from '../../Components/Preview-Collection/Preview-collection'
-class ShopPage extends React.Component{
-    constructor(props){
-        super(props)
+// class ShopPage extends React.Component{
+//     constructor(props){
+//         super(props)
 
-        this.state={
-            collections:SHOP_DATA
-        }
-    }
-    render(){
-        const {collections} = this.state;
-        return(
-            <div className='shop-page'>
-                {
-                    collections.map(({id,...otherCollectionProps}) =>(
-                        <CollectionPreview key={id} {...otherCollectionProps}/>
-                    ))
-                }
+//         this.state={
+//             collections:SHOP_DATA
+//         }
+//     }
+//     render(){
+//         const {collections} = this.state;
+//         return(
+//             <div className='shop-page'>
+//                 {
+//                     collections.map(({id,...otherCollectionProps}) =>(
+//                         <CollectionPreview key={id} {...otherCollectionProps}/>
+//                     ))
+//                 }
                 
-            </div>
-        )
-    }
+//             </div>
+//         )
+//     }
+// }
+
+const ShopPage =() =>{
+    const[collections] = useState (SHOP_DATA)
+    return(
+        <div className='shop-page'>
+            {
+                collections.map(({id,...otherCollectionProps}) =>(
+                    <CollectionPreview key={id} {...otherCollectionProps}/>
+                ))
+            }
+            
+        </div>
+    )
 }
 
 export default ShopPage;

178c8c31eab39d31a508cf475ec1880037c540c1
Kishan2029 <63163246+Kishan2029@users.noreply.github.com>
2021-04-30 14:28:38 +0530

    Userstate is added in app.js; Userstate will be updated after sign in and will become null when user log out

--- src/App.js
+++ src/App.js
@@ -4,23 +4,42 @@ import Homepage from './Pages/Homepage/Homepage'
 import ShopPage from './Pages/ShopPage/ShopPage'
 import Header from './Components/Header/Header'
 import SignInAndSignUp from './Pages/SignUp-SignIn/SignUp-SignIn'
+import {auth} from './Firebase/Firebase'
+
+
+
 import './App.css'
 
 
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      currentUser:null
+    }
+  }
 
+  componentDidMount(){
+     auth.onAuthStateChanged(user => {
+       this.setState({currentUser:user})
+       console.log(user);
+     })
+  }
 
-function App() {
-  return (
-    <div>
-      <Header/>
-      <Switch>
-          <Route exact path='/' component={Homepage}/>
-          <Route path='/shop' component={ShopPage}/>
-          <Route path='/signin' component={SignInAndSignUp}/>
-      </Switch>
-        
-    </div>
-  );
+  render(){
+    return (
+      <div>
+        <Header/>
+        <Switch>
+            <Route exact path='/' component={Homepage}/>
+            <Route path='/shop' component={ShopPage}/>
+            <Route path='/signin' component={SignInAndSignUp}/>
+        </Switch>
+          
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
